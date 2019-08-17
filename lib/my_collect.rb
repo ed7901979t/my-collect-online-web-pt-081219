0c973715cd8a1e101666d692a4058bbae033f9bd
clean-command-1851 <ed7901979t@gmail.com>
2019-08-17 18:41:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,3 @@
-collection = ['Joh Smith', 'Joohn Doe', 'Eric Smith', 'Cool Dee']
-my_collect(collection) do |lang|
-  lang.reverse
-end
+collection = ["Joh Smith", "Joohn Doe", "Eric Smith", "Cool Dee"]
+ 
 
