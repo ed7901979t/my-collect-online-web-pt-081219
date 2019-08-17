1c239b313fc8fd93feb72ee1ad2ad5cdc703f78c
clean-command-1851 <ed7901979t@gmail.com>
2019-08-17 18:40:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,4 @@
-collection = ['ruby', 'javascript', 'python', 'objective-c']
+collection = ['Joh Smith', 'Joohn Doe', 'Eric Smith', 'Cool Dee']
 my_collect(collection) do |lang|
   lang.reverse
 end
