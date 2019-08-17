511aef6b350714704c3e876cbab75a0657629af1
clean-command-1851 <ed7901979t@gmail.com>
2019-08-17 18:42:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,11 @@
 collection = ["Joh Smith", "Joohn Doe", "Eric Smith", "Cool Dee"]
- 
+ def my_collect(array)
+  i = 0
+  new_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  i += 1
+  end
+  new_collection
+end
 
